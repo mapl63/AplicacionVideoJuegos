ae50a24b8dd4b19293941ddbdeb3da5bd17d91d3
2daw <mapl63@educa.madrid.org>
2025-11-13 18:39:21 +0100

    rama base de datos cambios

---
+++ src/main/java/com/example/aplicacionvideojuegos/videoJuegos/titulares/models/Titular.java
@@ -0,0 +1,18 @@
+package com.example.aplicacionvideojuegos.videoJuegos.titulares.models;
+
+
+import lombok.*;
+
+@Builder
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TITULAREs")
+public class Titular {
+
+    @Id
+
+}
---
+++ src/main/java/com/example/aplicacionvideojuegos/videoJuegos/titulares/repositories/TitularesRepository.java
@@ -0,0 +1,7 @@
+package com.example.aplicacionvideojuegos.videoJuegos.titulares.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TitularesRepository {
+}
