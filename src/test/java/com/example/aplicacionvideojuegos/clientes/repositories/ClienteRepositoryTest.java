59812ff10e6dace985395355418b8d5e8dbcf122
2daw <mapl63@educa.madrid.org>
2025-11-18 20:43:20 +0100

    cambios

--- src/test/java/com/example/aplicacionvideojuegos/clientes/repositories/ClienteRepositoryTest.java
+++ src/test/java/com/example/aplicacionvideojuegos/clientes/repositories/ClienteRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @Slf4j
+
 @DataJpaTest
 class ClienteRepositoryTest {
 
